801467a4023f65d2b12e993a30a5f3c3bcb0bf37
Vinh <pquangvinh1999@gmail.com>
2025-11-03 23:28:15 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import { materials } from "./data/materials";
+import InventoryTable from "./components/InventoryTable";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <InventoryTable data={materials} />
+    </div>
+  );
+}
+
+export default App;
 
