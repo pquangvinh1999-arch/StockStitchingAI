79c534d65debea24f6d3788e08039b2ff6c38551
Vinh <pquangvinh1999@gmail.com>
2025-11-03 23:27:40 +0700

    Update materials.js

--- src/data/materials.js
+++ src/data/materials.js
@@ -1,1 +1,14 @@
-
+export const materials = [
+  { viTri: "02-02-03-A", maVatLieu: "R28928300007", size: 10, soLuong: 50000, ngayGR: "23/10/2025" },
+  { viTri: "13-01-02-A", maVatLieu: "C4403500001", size: 120, soLuong: 3500, ngayGR: "25/04/2022" },
+  { viTri: "13-01-02-A", maVatLieu: "C4408100072", size: 145, soLuong: 64, ngayGR: "26/09/2024" },
+  { viTri: "13-01-02-B", maVatLieu: "C440100007", size: 80, soLuong: 123, ngayGR: "08/02/2022" },
+  { viTri: "13-01-02-B", maVatLieu: "C4406303079", size: 110, soLuong: 428, ngayGR: "25/01/2022" },
+  { viTri: "13-01-02-B", maVatLieu: "C4408100001", size: 130, soLuong: 2053, ngayGR: "23/04/2022" },
+  { viTri: "13-01-02-C", maVatLieu: "C440810007", size: 130, soLuong: 7, ngayGR: "05/01/2022" },
+  { viTri: "13-01-02-C", maVatLieu: "C4408100011", size: 130, soLuong: 962, ngayGR: "16/09/2025" },
+  { viTri: "13-01-02-C", maVatLieu: "C4408100012", size: 180, soLuong: 230, ngayGR: "12/05/2022" },
+  { viTri: "13-01-02-C", maVatLieu: "C4403100015", size: 125, soLuong: 1795, ngayGR: "14/09/2021" },
+  { viTri: "13-01-02-C", maVatLieu: "C4403300539", size: 130, soLuong: 807, ngayGR: "14/09/2021" },
+  { viTri: "13-01-02-C", maVatLieu: "C4405500178", size: 95, soLuong: 1455, ngayGR: "10/05/2021" },
+];
