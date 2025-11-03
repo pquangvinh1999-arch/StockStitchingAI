28f14ffe7817b6b3d07ad8fe03d6833851834cd9
Vinh <pquangvinh1999@gmail.com>
2025-11-04 00:06:02 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { materials } from "./data/materials";
 import InventoryTable from "./components/InventoryTable";
+import ChatAI from "./components/ChatAI";
+
+function App() {
+  return (
+    <div>
+      <h1>📦 Danh sách vật tư trong kho</h1>
+      {/* Bảng vật tư của mày */}
+      <ChatAI />
+    </div>
+  );
+}
 
 function App() {
   return (
